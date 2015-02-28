395c452cd2bb3a2fd2df917cc85877db14dac481
Sam Scott <avinchmc@gmail.com>
2015-02-28 22:54:08 +0000

    changed id from music to listenin

--- listenin.js
+++ listenin.js
@@ -21,7 +21,7 @@ google.load("feeds", "1");
       var feed = new google.feeds.Feed(url + (new Date).getTime());
       feed.load(function(result) {
         if (!result.error) {
-          var container = document.getElementById("music");
+          var container = document.getElementById("listenin");
           for (var i = 0; i < result.feed.entries.length; i++) {
             var entry = result.feed.entries[i];
             container.innerHTML = "<a href='" + entry.link + "'>" + entry.title + "</a>";
